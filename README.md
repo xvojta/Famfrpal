7cb84685eb5f8fe8fccec6f9b39cb4d8ad897161
Vojtech Ondracek <xvojta.ondracek@gmail.com>
2020-10-31 12:20:08 +0100

    Starter commit with plugin development worlkflow

--- README.md
+++
@@ -1,6 +0,0 @@
-# Slaparoo
-
-## To-Do:
-* Players can join during the countdown @JakubNj
-* Respawn the player in the arena @xvojta
-* Weapon is equiped in hand @zdenek-nemec
---
+++ src/com/xvojta/famfrpal/Famfrpal.java
@@ -0,0 +1,13 @@
+package com.xvojta.famfrpal;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Famfrpal extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        Bukkit.getLogger().info("Famfrpal plugin funguje jako vzdy");
+    }
+}
