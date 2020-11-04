68ae38725598182ef24f450bd432880a05c213d7
Vojtech Ondracek <xvojta.ondracek@gmail.com>
2020-11-04 19:54:03 +0100

    Teams manager added to handle teams in game. NOT TESTED

--- src/com/xvojta/famfrpal/FPTeamManager.java
+++ src/com/xvojta/famfrpal/FPTeamManager.java
@@ -5,19 +5,31 @@ import org.bukkit.scoreboard.Team;
 import java.util.HashMap;
 
 public class FPTeamManager {
-    public Team team;
-    public String[] playerNames;
-    public String teamName;
+    public HashMap<String, String[]> teamsNames;
+    public HashMap<String, Team> teams;
 
-    public FPTeam(String teamName, Team team, String[] playerNames)
+    public FPTeamManager(HashMap<String, String[]> teams)
     {
-        this.team = team;
-        this.playerNames = playerNames;
-        this.teamName = teamName;
+        this.teamsNames = teams;
+    }
+
+    public String[] getTeamsNames()
+    {
+        return (String[]) teamsNames.keySet().toArray();
     }
 
     public Team getTeamByPlayer(String playerName)
     {
-
+        for (String[] s : teamsNames.values())
+        {
+            for (int i = 0; i < s.length; i++)
+            {
+                if (s[i] == playerName)
+                {
+                    return teams.get(i);
+                }
+            }
+        }
+        return null;
     }
 }
--- src/com/xvojta/famfrpal/Famfrpal.java
+++ src/com/xvojta/famfrpal/Famfrpal.java
@@ -1,24 +1,21 @@
 package com.xvojta.famfrpal;
 
-import net.minecraft.server.v1_16_R2.PlayerConnection;
-import org.apache.logging.log4j.core.net.Priority;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.advancement.Advancement;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.v1_16_R2.entity.CraftPlayer;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.*;
@@ -28,14 +25,26 @@ import java.util.logging.Logger;
 
 public class Famfrpal extends JavaPlugin implements Listener
 {
+    private static final int ENDERDRAGONSCORE = 100;
+    private static final int PLAYERBOUNTY = 5;
+    private static final int ELDERGUARDIANSCORE = 75;
+    private static final int WITHERSCORE = 75;
+    public static Famfrpal Instance;
     public Logger LOGGER = Bukkit.getLogger();
     public Scoreboard scoreboard;
-    public Team team;
     public Objective objective;
+    public FPTeamManager teamManager;
 
     @Override
     public void onEnable()
     {
+        //fill teams
+        HashMap<String, String[]> teams = new HashMap<String, String[]>();
+        teams.put("TestTeam", new String[]{"xvojta", "NEMO_CZ"});
+        teamManager = new FPTeamManager(teams);
+
+        Instance = this;
+
         Bukkit.getLogger().info("Famfrpal plugin funguje jako vzdy");
 
         PluginManager manager = getServer().getPluginManager();
@@ -53,7 +62,16 @@ public class Famfrpal extends JavaPlugin implements Listener
 
         if (label.equalsIgnoreCase("fp"))
         {
-            LOGGER.info("Famfrpal commmand");
+            if (arguments[0] != null)
+            {
+                if (arguments[0] == "addScore")
+                {
+                    if (arguments[1] != null && arguments[2] != null)
+                    {
+                        addScore(getServer().getPlayer(arguments[1]), Integer.parseInt(arguments[2]));
+                    }
+                }
+            }
             return true;
         }
         return false;
@@ -65,11 +83,27 @@ public class Famfrpal extends JavaPlugin implements Listener
 
         LOGGER.info("player joined FP");
 
-        //here the error appers. I need to add something like callLater
-        team.addEntry(player.getName());
-        player.setScoreboard(scoreboard);
-        Score score = objective.getScore(player.getName());
-        score.setScore(0);
+        Bukkit.getScheduler().runTaskLater(this, new Runnable() {
+            @Override
+            public void run() {
+                Famfrpal.Instance.setScoreboardToPlayer(player);
+            }
+        }, 20L);
+    }
+    //here the error appers. I need to add something like callLater
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event)
+    {
+        Player killedPlayer = event.getEntity();
+        Player killer = killedPlayer.getKiller();
+
+        if (killer != null)
+        {
+            addScore(killer, PLAYERBOUNTY);
+        }
+        
+        addScore(killedPlayer, -PLAYERBOUNTY);
     }
 
     @EventHandler
@@ -78,20 +112,54 @@ public class Famfrpal extends JavaPlugin implements Listener
         LivingEntity entity = event.getEntity();
         Player player = entity.getKiller();
 
-        if(player != null) {
+        if(player != null)
+        {
             if (event.getEntityType() == EntityType.ENDER_DRAGON)
             {
-
+                addScore(player, ENDERDRAGONSCORE);
+            }
+            if (event.getEntityType() == EntityType.ELDER_GUARDIAN)
+            {
+                addScore(player, ELDERGUARDIANSCORE);
+            }
+            if (event.getEntityType() == EntityType.WITHER)
+            {
+                addScore(player, WITHERSCORE);
             }
         }
     }
 
-    private void craeteScoreBoard(World world) {
+    @EventHandler
+    public void onPlayerDoneAdvancement(PlayerAdvancementDoneEvent event)
+    {
+        Player player = event.getPlayer();
+        event.getAdvancement().getCriteria().forEach((a) -> player.sendMessage(a));
+    }
+
+    public void addScore(Player player, int amount)
+    {
+        Score score = objective.getScore(player.getName());
+        int newScore = score.getScore() + amount;
+        score.setScore(newScore);
+    }
+
+    public void setScoreboardToPlayer(Player player)
+    {
+        teamManager.getTeamByPlayer(player.getName()).addEntry(player.getName());
+        player.setScoreboard(scoreboard);
+        Score score = objective.getScore(player.getName());
+        score.setScore(0);
+    }
+
+    private void craeteScoreBoard(World world)
+    {
         ScoreboardManager manager = Bukkit.getScoreboardManager();
         scoreboard = manager.getNewScoreboard();
-        objective = scoreboard.registerNewObjective("test", "dummy");
+        objective = scoreboard.registerNewObjective("test", "dummy", "Score");
         objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-        objective.setDisplayName("Score");
-        team = scoreboard.registerNewTeam("Players");
+
+        for (String i : teamManager.getTeamsNames()) {
+            teamManager.teams.put(i, scoreboard.registerNewTeam(i));
+        }
     }
 }
